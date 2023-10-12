e6fb135942634889084209a9b69a78ad6f55d3b8
takumi <yoshida.fm.0626@gmail.com>
2023-10-13 07:13:54 +0900

    機能一覧　PCにて表示が崩れる　を修正

--- frontend/src/pages/Customer/Customer.jsx
+++ frontend/src/pages/Customer/Customer.jsx
@@ -9,75 +9,76 @@ const Customer = () => {
             <Topbar />
             <div className="homeContainer">
                 <Hamburger />
-            </div>
-            <div className="customer">
-                <h2 className="customerTitle">機能一覧</h2>
-                <ul className="customerList">
-                    <li className="CstomerItem">新規アカウント作成</li>
-                    <li className="CstomerItem">ログイン・ログアウト</li>
-                </ul>
-                <hr />
-                <h3 className="customerListTitle">トップバー</h3>
-                <ul className="customerList">
-                    <li className="CstomerItem">メニュー表示（スマホ閲覧時のみ）</li>
-                    <li className="CstomerItem">ユーザー名検索</li>
-                    <li className="CstomerItem">メッセージ閲覧</li>
-                    <li className="CstomerItem">アイコンクリックでプロフィール画面へ遷移</li>
-                </ul>
-                <hr />
 
-                <h3 className="customerListTitle">ホーム画面</h3>
-                <ul className="customerList">
-                    <li className="CstomerItem">リアルタイム通信</li>
-                    <li className="CstomerItem">テキスト投稿</li>
-                    <li className="CstomerItem">画像投稿</li>
-                    <li className="CstomerItem">自分の投稿内容の閲覧</li>
-                    <li className="CstomerItem">フォローした人の投稿内容の閲覧</li>
-                    <li className="CstomerItem">いいね</li>
-                    <li className="CstomerItem">投稿にコメント</li>
-                    <li className="CstomerItem">投稿をブックマーク</li>
-                    <li className="CstomerItem">自分の投稿を編集</li>
-                    <li className="CstomerItem">自分の投稿を削除</li>
-                    <li className="CstomerItem">ページネーション（「インクリメント」というユーザーをフォローすると確認できます。）</li>
-                </ul>
-                <hr />
+                <div className="customer">
+                    <h2 className="customerTitle">機能一覧</h2>
+                    <ul className="customerList">
+                        <li className="CstomerItem">新規アカウント作成</li>
+                        <li className="CstomerItem">ログイン・ログアウト</li>
+                    </ul>
+                    <hr />
+                    <h3 className="customerListTitle">トップバー</h3>
+                    <ul className="customerList">
+                        <li className="CstomerItem">メニュー表示（スマホ閲覧時のみ）</li>
+                        <li className="CstomerItem">ユーザー名検索</li>
+                        <li className="CstomerItem">メッセージ閲覧</li>
+                        <li className="CstomerItem">アイコンクリックでプロフィール画面へ遷移</li>
+                    </ul>
+                    <hr />
 
-                <h3 className="customerListTitle">メニューバー</h3>
-                <ul className="customerList">
-                    <li className="CstomerItem">各メニューへ遷移</li>
-                    <li className="CstomerItem">おすすめのユーザーの表示（ランダム）</li>
-                </ul>
-                <hr />
+                    <h3 className="customerListTitle">ホーム画面</h3>
+                    <ul className="customerList">
+                        <li className="CstomerItem">リアルタイム通信</li>
+                        <li className="CstomerItem">テキスト投稿</li>
+                        <li className="CstomerItem">画像投稿</li>
+                        <li className="CstomerItem">自分の投稿内容の閲覧</li>
+                        <li className="CstomerItem">フォローした人の投稿内容の閲覧</li>
+                        <li className="CstomerItem">いいね</li>
+                        <li className="CstomerItem">投稿にコメント</li>
+                        <li className="CstomerItem">投稿をブックマーク</li>
+                        <li className="CstomerItem">自分の投稿を編集</li>
+                        <li className="CstomerItem">自分の投稿を削除</li>
+                        <li className="CstomerItem">ページネーション（「インクリメント」というユーザーをフォローすると確認できます。）</li>
+                    </ul>
+                    <hr />
 
-                <h3 className="customerListTitle">プロフィール</h3>
-                <ul className="customerList">
-                    <li className="CstomerItem">カバー画像の編集</li>
-                    <li className="CstomerItem">プロフィール画像の編集</li>
-                    <li className="CstomerItem">プロフィール内容の編集</li>
-                    <li className="CstomerItem">自分の投稿のみの閲覧</li>
-                    <li className="CstomerItem">フォロー</li>
-                    <li className="CstomerItem">フォロー解除</li>
-                    <li className="CstomerItem">メッセージ</li>
-                </ul>
-                <hr />
+                    <h3 className="customerListTitle">メニューバー</h3>
+                    <ul className="customerList">
+                        <li className="CstomerItem">各メニューへ遷移</li>
+                        <li className="CstomerItem">おすすめのユーザーの表示（ランダム）</li>
+                    </ul>
+                    <hr />
 
-                <h3 className="customerListTitle">メッセージ</h3>
-                <ul className="customerList">
-                    <li className="CstomerItem">リアルタイム通信</li>
-                    <li className="CstomerItem">受信一覧</li>
-                    <li className="CstomerItem">メッセージ一覧</li>
-                    <li className="CstomerItem">メッセージ送信</li>
-                    <li className="CstomerItem">未読数表示</li>
-                    <li className="CstomerItem">既読</li>
-                </ul>
-                <hr />
+                    <h3 className="customerListTitle">プロフィール</h3>
+                    <ul className="customerList">
+                        <li className="CstomerItem">カバー画像の編集</li>
+                        <li className="CstomerItem">プロフィール画像の編集</li>
+                        <li className="CstomerItem">プロフィール内容の編集</li>
+                        <li className="CstomerItem">自分の投稿のみの閲覧</li>
+                        <li className="CstomerItem">フォロー</li>
+                        <li className="CstomerItem">フォロー解除</li>
+                        <li className="CstomerItem">メッセージ</li>
+                    </ul>
+                    <hr />
 
-                <h3 className="customerListTitle">ブックマーク</h3>
-                <ul className="customerList">
-                    <li className="CstomerItem">投稿をブックマーク</li>
-                    <li className="CstomerItem">ブックマークを解除</li>
-                    <li className="CstomerItem">ブックマークした投稿を全て閲覧</li>
-                </ul>
+                    <h3 className="customerListTitle">メッセージ</h3>
+                    <ul className="customerList">
+                        <li className="CstomerItem">リアルタイム通信</li>
+                        <li className="CstomerItem">受信一覧</li>
+                        <li className="CstomerItem">メッセージ一覧</li>
+                        <li className="CstomerItem">メッセージ送信</li>
+                        <li className="CstomerItem">未読数表示</li>
+                        <li className="CstomerItem">既読</li>
+                    </ul>
+                    <hr />
+
+                    <h3 className="customerListTitle">ブックマーク</h3>
+                    <ul className="customerList">
+                        <li className="CstomerItem">投稿をブックマーク</li>
+                        <li className="CstomerItem">ブックマークを解除</li>
+                        <li className="CstomerItem">ブックマークした投稿を全て閲覧</li>
+                    </ul>
+                </div>
             </div>
         </>
 
